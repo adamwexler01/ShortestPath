4cdb22999fa876101fb7816268b26f95338d9e07
adamwexler01 <adamwexler01@gmail.com>
2016-04-23 18:32:02 -0400

    Updating the Graph.h File

--- Graph.h
+++ Graph.h
@@ -9,35 +9,40 @@ using namespace std;
 
 struct Vertex {
 	string value;
-	int *powers;							//Add the largest Subset in Increasing Order
-};
-
-struct Edge{
-	Vertex *source;
-	Vertex *destination;
-	int weight;
+	int *powers;							//Holds the set of magi
 };
 
 //Class structure of Graph
 
 class Graph{
 private:
-
+	Vertex *realms;
 
 public:
-	Graph();
+	Graph(int size);
 	~Graph();
+	void addRealm(int src, string val, int pows[]);
+	void addEdge();
 	void Dijkstras();
-
 };
 
-Graph::Graph(){
+Graph::Graph(int size){
 	//Setting up Graph Data Structure
+	
+	realms = new Vertex[size];
+
 }
 
 Graph::~Graph(){
 	//Destruct the Structure
+	delete realms;
 }
+
+void Graph::addRealm(int src, string val, int pows[]){
+	realms[src].value = val;
+	realms[src].powers = pows;
+}
+
 
 void Graph::Dijkstras(){
 	//Implementing Dijkstras Algorithm
@@ -46,8 +51,13 @@ void Graph::Dijkstras(){
 
 
 
-
-
+//Might not be used because Edit Distance covers that
+//
+// struct Edge{
+// 	Vertex *source;
+// 	Vertex *destination;
+// 	int weight;
+// };
 
 
 
