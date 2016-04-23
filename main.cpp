aa3eb9704db2768fc29102b32a104e72f91161dd
mondily <Mondily@Amandas-MacBook-Pro.local>
2016-04-23 18:51:43 -0400

    ./main < input

--- main.cpp
+++ main.cpp
@@ -21,41 +21,6 @@ private:
 public:
 };
 
-int min(int a,int b) {
-   return ((a < b) ? a : b);
-}
-
-int diff(char a, char b) {
-  //if characters not same, return 1 to add to minimum # changes needed
-  return ((a == b) ? 0 : 1);
-}
-
-int minChanges(string str1, string str2) {
-  int s1 = str1.length();
-  int s2 = str2.length();
-
-  //edit distance table to compute minimum # of changes
-  int table[s1+1][s2+1];
- 
-  for (int i = 0; i < s1+1; i++) {        // 0 1 2 3 4 5
-    table[i][0] = i;                          // 1
-  }                                       // 2
-                                          // 3
-  for (int i = 1; i < s2+1; i++) {        // 4
-    table[0][i] = i;                          // 5
-  }
- 
-  for (int i = 1; i < s1+1; i++) {
-    for (int j = 1; j < s2+1; j++) {
-      int less = min(table[i-1][j], table[i][j-1]) + 1;
-      table[i][j] = min(less, table[i-1][j-1] + diff(str1[i-1], str2[j-1]));
-    }
-  }
-  
-  //minimum number of changes need to go from str1 to str2
-  return table[s1][s2];
-}
-
 
 int main(int argc, const char * argv[]) {
   
